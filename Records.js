42579ae3033dd40ea5658f559292a4d2dc5134f3
denisezou <dzou@u.northwestern.edu>
2019-07-06 12:15:14 -0500

    Create Records.js

---
+++ Records.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+
+import API from "../../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../Grid";
+import { List, ListItem } from "../List";
+import { Input, TextArea, FormBtn } from "../Form";
+
+
+import "./style.css";
+
+class Record extends Component {
+  state = {
+    books: [],
+    q: "",
+    message: "Search For A Book To Begin!"
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  getBooks = () => {
+    API.getBooks(this.state.q)
+      .then(res =>
+        this.setState({
+          books: res.data
+        })
+      )
+      .catch(() =>
+        this.setState({
+          books: [],
+          message: "No New Books Found, Try a Different Query"
+        })
+      );
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.getBooks();
+  };
+
+  handleBookSave = id => {
+    const book = this.state.books.find(book => book.id === id);
+
+    API.saveBook({
+      googleId: book.id,
+      title: book.volumeInfo.title,
+      subtitle: book.volumeInfo.subtitle,
+      link: book.volumeInfo.infoLink,
+      authors: book.volumeInfo.authors,
+      description: book.volumeInfo.description,
+      image: book.volumeInfo.imageLinks.thumbnail
+    }).then(() => this.getBooks());
+  };
+
+  render() {
+    return(
+      console.log(
+        book.id + 
+        book.volumeInfo.title + 
+        book.volumeInfo.subtitle +
+        book.volumeInfo.infoLink +
+        book.volumeInfo.authors.join(", ")+
+        book.volumeInfo.description+
+        book.volumeInfo.imageLinks.thumbnail
+
+    ))
+  }
+}
+export default Record
